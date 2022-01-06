c8ddf71146d9a0f8c12e72f898212fe0850f335d
SpeedyM282 <a.rizakulov@student.inha.uz>
2022-01-06 14:20:01 +0500

    refactor: change some code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ module.exports = function toReadable(number) {
   if (number < 20) return num[number];
   var digit = number % 10;
 
-  if (number < 100) return tens[~~(number / 10) - 2] + (digit ? '-' + num[digit] : '');
+  if (number < 100) return tens[~~(number / 10) - 2] + (digit ? num[digit] : '');
   if (number < 1000) return num[~~(number / 100)] + ' hundred' + (number % 100 == 0 ? '' : ' ' + toReadable(number % 100));
   return toReadable(~~(number / 1000)) + ' thousand' + (number % 1000 != 0 ? ' ' + toReadable(number % 1000) : "");
 }
